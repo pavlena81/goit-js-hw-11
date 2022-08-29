414dcbfef72e6e813091acd1724acd4af14761d6
Olena Nesteruk <pavlena81@gmail.com>
2022-08-29 19:02:41 +0300

    add axios

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 import './css/styles.css';
 
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+import axios from "axios";
+
+axios.defaults.baseURL =
+  'https://pixabay.com/api?key=29581970-ca9e55c9ea9a40620816915df';
+
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
