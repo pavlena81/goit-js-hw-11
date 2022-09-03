9602407c0db37a90f1782566deeec3a1d8e0e1ad
Olena Nesteruk <pavlena81@gmail.com>
2022-09-04 01:57:33 +0300

    adds render

--- src/index.js
+++ src/index.js
@@ -3,13 +3,40 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import axios from "axios";
+import renderGallery from './renderGallery';
+
+
+
 
 axios.defaults.baseURL =
-  'https://pixabay.com/api?key=29581970-ca9e55c9ea9a40620816915df';
+    'https://pixabay.com/api?key=29581970-ca9e55c9ea9a40620816915df';
+  
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  galleryEl: document.querySelector('.gallery'),
+  btnLoadMore: document.querySelector('.load-more'),
+  
+};
 
-async function getUser() {
+let page = 1;
+let query = '';
+
+refs.searchForm.addEventListener('submit', onSearchForm);
+
+function onSearchForm(e) {
+    e.preventDefault();
+    
+    //if (query === e.target.elements.query.value) return;
+
+    query = e.target.value.trim();
+
+    if (!query) return;
+
+
+}
+async function fetch() {
   try {
-    const response = await axios.get('/user?ID=12345');
+    const response = await axios.get(`?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`);
     console.log(response);
   } catch (error) {
     console.error(error);
---
+++ src/renderGallery.js
@@ -0,0 +1,31 @@
+export default function renderGallery(images) {
+     
+    const markup = images
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return `
+    <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+            <a href="${largeImageURL}">
+            <p class="info-item">
+            <b>Likes</b>${likes}
+            </p>
+            <p class="info-item">
+             <b>Views</b>${views}
+            </p>
+            <p class="info-item">
+            <b>Comments</b>${comments}
+            </p>
+            <p class="info-item">
+            <b>Downloads</b>${downloads}            
+            </p>
+        </div>
+    </div>
+      `;
+        })
+    .join("");
+  return markup;
+//  return countryList.innerHTML = markup;
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+};
+
