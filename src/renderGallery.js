5deb246eb860bc19a536703b229aa7a938e9ff9a
Olena Nesteruk <pavlena81@gmail.com>
2022-09-05 20:33:11 +0300

    fixed styles

--- src/renderGallery.js
+++ src/renderGallery.js
@@ -8,16 +8,20 @@ export default function renderGallery(images) {
         <img  class="photo__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
         <div class="info">
             <p class="info-item">
-            <b>Likes</b>${likes}
+            <b>Likes</b>
+            ${likes}
             </p>
             <p class="info-item">
-             <b>Views</b>${views}
+             <b>Views</b>
+             ${views}
             </p>
             <p class="info-item">
-            <b>Comments</b>${comments}
+            <b>Comments</b>
+            ${comments}
             </p>
             <p class="info-item">
-            <b>Downloads</b>${downloads}            
+            <b>Downloads</b>
+            ${downloads}
             </p>
         </div>
     </div>
